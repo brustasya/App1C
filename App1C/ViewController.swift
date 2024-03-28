dba8498dd164c34c1813ece07513f6c4b42c2619
brustasya <sabobruskina@edu.hse.ru>
2024-03-28 20:38:08 +0300

    Initial Commit

---
+++ App1C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App1C
+//
+//  Created by Станислава on 28.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
